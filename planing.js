7dd5cf79110753e4198d42c4a1451f018f1e31b0
alokk <alokkumar0723@gmail.com>
2021-10-04 15:01:21 +0530

    First commit

---
+++ planing.js
@@ -0,0 +1,21 @@
+//Book my Planning
+
+//-> We need a reusable carousel
+//->HERO CAROUSEL
+//->COMMON CAROUSEL
+//->CAST CAROUSEL (small carousel)
+
+//Pages
+
+//-> Home Pages
+//-> Movie Page
+//-> Categories Page
+//->Auth Page
+//-> Razorpay payment gateway
+
+//Plan how we are building it?
+
+//Layouts
+//Routes
+//Pages
+//Components
